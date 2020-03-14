0cd3ad393f023e9bafa571981ee2fe8a010e3f30
A. Mattson Gallagher <amg@a-m-g.org>
2020-03-14 16:49:20 -0400

    Add arrow key navigation

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,7 +1,7 @@
-var gallery;
-var nav;
+var gallery,
+    nav;
 
-function setup() {
+window.addEventListener("DOMContentLoaded", () => {
   // Setup collections
   gallery = document.querySelectorAll("div#gallery li");
   nav     = document.querySelectorAll("nav#details li");
@@ -9,24 +9,64 @@ function setup() {
   change();
   // Add change listeners
   window.addEventListener("hashchange", change);
+  window.addEventListener("keydown", handleKey);
   // Add transition
   // (prevents fade if hash already in URL)
   setTimeout(() => {
     gallery.forEach(g => g.style.transition = "opacity .1s ease");
-  }, 0);
+  }, 200);
+});
+
+
+
+function current() {
+  return (window.location.hash != "")
+    ? Number(window.location.hash.replace("#", ""))
+    : 1;
 }
+
+function index() {
+  return (current() - 1);
+}
+
+function setHash(h) {
+  window.location.hash = "#" + ("00" + h).slice(-2);
+}
+
+
 
 function change() {
-  // Get index
-  index = (window.location.hash != "")
-          ? (Number(window.location.hash.replace("#", "")) - 1)
-          : 0;
   // Change navigation
   nav.forEach(n => n.classList.remove("viewing"));
-  nav[index].classList.add("viewing");
+  nav[index()].classList.add("viewing");
   // Change image
   gallery.forEach(g => g.classList.remove("viewing"));
-  gallery[index].classList.add("viewing");
+  gallery[index()].classList.add("viewing");
 }
 
-window.addEventListener("DOMContentLoaded", setup);
+function next() {
+  let nextHash = current() + 1;
+  if (nextHash <= gallery.length) {
+    setHash(nextHash);
+  }
+}
+
+function prev() {
+  let prevHash = current() - 1;
+  if (prevHash > 0) {
+    setHash(prevHash);
+  }
+}
+
+
+
+function handleKey(e) {
+  e = e || window.event;
+  if (e.keyCode == '39') {
+    // right arrow
+    next();
+  } else if (e.keyCode == '37') {
+    // left arrow
+    prev();
+  }
+}
