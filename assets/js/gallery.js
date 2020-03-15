119de5c03d42c154c35d0556a246b1f41b226fba
A. Mattson Gallagher <amg@a-m-g.org>
2020-03-15 14:47:04 -0400

    Add image pager (left/right arrows)

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,14 +1,18 @@
 // Setup DOM references
 const GALLERY = document.querySelector("div#gallery"),
       IMAGES  = document.querySelectorAll("div#gallery li"),
-      NAV     = document.querySelectorAll("nav#details li");
+      NAV     = document.querySelectorAll("nav#details li"),
+      A_PREV  = document.querySelector("nav#page-images a.prev"),
+      A_NEXT  = document.querySelector("nav#page-images a.next");
 
 window.addEventListener("DOMContentLoaded", () => {
   // Show first image
   change();
+
   // Add change listeners
   window.addEventListener("hashchange", change);
   window.addEventListener("keydown", handleKey);
+
   // Add transition
   // (prevents fade if hash already in URL)
   setTimeout(() => {
@@ -17,10 +21,27 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function change() {
-  let index = current() - 1;
+  let cardinal = current();
+  let index    = cardinal - 1;
+
   // Change navigation
   NAV.forEach(n => n.classList.remove("viewing"));
   NAV[index].classList.add("viewing");
+
+  // Change pager targets
+  if (index > 0) {
+    A_PREV.style.visibility = "";
+    A_PREV.href = hash(cardinal - 1);
+  } else {
+    A_PREV.style.visibility = "hidden";
+  }
+  if (index < (IMAGES.length - 1)) {
+    A_NEXT.style.visibility = "";
+    A_NEXT.href = hash(cardinal + 1);
+  } else {
+    A_NEXT.style.visibility = "hidden";
+  }
+
   // Change image
   IMAGES.forEach(i => i.classList.remove("viewing"));
   IMAGES[index].classList.add("viewing");
@@ -36,8 +57,8 @@ function current() {
     : 1;
 }
 
-function setHash(h) {
-  window.location.hash = "#" + ("00" + h).slice(-2);
+function hash(n) {
+  return ("#" + ("00" + n).slice(-2));
 }
 
 function handleKey(e) {
@@ -57,16 +78,14 @@ function handleKey(e) {
 // Navigation
 
 function nextImage() {
-  let nextHash = current() + 1;
-  if (nextHash <= IMAGES.length) {
-    setHash(nextHash);
+  if ((current() + 1) <= IMAGES.length) {
+    window.location.hash = hash(current() + 1);
   }
 }
 
 function prevImage() {
-  let prevHash = current() - 1;
-  if (prevHash > 0) {
-    setHash(prevHash);
+  if ((current() - 1) > 0) {
+    window.location.hash = hash(current() - 1);
   }
 }
 
