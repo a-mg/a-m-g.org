a-mg/a-m-g.org

commit 11e506094fb56222404228c38a3a323f5e62e323

Improve text color, light/dark mode handling
The previous mix-blend-mode approach was unstable, and led to some ugly
results with text over images when they weren't an even gray. It looks a
lot better if we use rgba values.

This change introduces two sets of colors (dark/light text): setting
them by default in light and dark mode, and making them configurable on
a per-image basis in the gallery (for full images).

assets/css/default.css
@@ -1,23 +1,41 @@
 /* Variables */
 
 :root {
-  --c-bg:        white;
-  --c-bg-view:   rgba(0, 0, 0, .1);
-  --c-text:      white;
-  --c-text-off:  rgba(255, 255, 255, .4);
-  --c-line:      rgba(255, 255, 255, .2);
-  --c-shadow:    rgba(0, 0, 0, .2);
+  --c-bg:       white;
+  --c-bg-view:  rgba(0, 0, 0, .1);
+  --c-shadow:   rgba(0, 0, 0, .2);
 
-  --f-size:      15px;
-  --f-serif:     "Times New Roman", Times, serif;
-  --f-sans:      Arial, sans-serif;
+  /* dark values (light mode) */
+  --d-text:     black;
+  --d-text-off: rgba(0, 0, 0, .4);
+  --d-line:     rgba(0, 0, 0, .2);
+  /* light values (dark mode) */
+  --l-text:     white;
+  --l-text-off: rgba(255, 255, 255, .4);
+  --l-line:     rgba(255, 255, 255, .2);
+  /* defaults */
+  --x-text:     var(--d-text);
+  --x-text-off: var(--d-text-off);
+  --x-line:     var(--d-line);
+  /* concrete values */
+  --c-text:     var(--x-text);
+  --c-text-off: var(--x-text-off);
+  --c-line:     var(--x-line);
+
+  --f-size:     15px;
+  --f-serif:    "Times New Roman", Times, serif;
+  --f-sans:     Arial, sans-serif;
 }
 
 @media (prefers-color-scheme: dark) {
   :root {
-    --c-bg:      rgb(25.5, 25.5, 25.5);
-    --c-bg-view: rgb(25.5, 25.5, 25.5);
-    --c-shadow:  rgba(0, 0, 0, 1);
+    --c-bg:       rgb(25.5, 25.5, 25.5);
+    --c-bg-view:  rgb(25.5, 25.5, 25.5);
+    --c-shadow:   rgba(0, 0, 0, 1);
+
+    --x-text:     var(--l-text);
+    --x-text-off: var(--l-text-off);
+    --x-line:     var(--l-line);
   }
 }
 
@@ -46,11 +64,6 @@ body {
 
 
 /* Basic text styles */
-
-nav, section, p#waiting {
-  /* makes text light/dark depending on background */
-  mix-blend-mode: difference;
-}
 
 a {
   text-decoration: none;

assets/js/gallery.js
@@ -1,5 +1,6 @@
 // Object references
-const GALLERY = document.querySelector("div#gallery"),
+const ROOT    = document.documentElement,
+      GALLERY = document.querySelector("div#gallery"),
       IMAGES  = document.querySelectorAll("div#gallery li"),
       NAV     = document.querySelectorAll("nav#details li"),
       A_PREV  = document.querySelector("nav#page-images li.prev"),
@@ -42,6 +43,21 @@ function change(index) {
   // Change pager targets
   A_PREV.dataset.index = inRange(i = index - 1) ? i : "";
   A_NEXT.dataset.index = inRange(i = index + 1) ? i : "";
+
+  // Change text style
+  if (IMAGES[index].classList.contains("dark")) {
+    ROOT.style.setProperty("--c-text",     "var(--d-text)");
+    ROOT.style.setProperty("--c-text-off", "var(--d-text-off)");
+    ROOT.style.setProperty("--c-line",     "var(--d-line)");
+  } else if (IMAGES[index].classList.contains("light")) {
+    ROOT.style.setProperty("--c-text",     "var(--l-text)");
+    ROOT.style.setProperty("--c-text-off", "var(--l-text-off)");
+    ROOT.style.setProperty("--c-line",     "var(--l-line)");
+  } else {
+    ROOT.style.setProperty("--c-text",     "var(--x-text)");
+    ROOT.style.setProperty("--c-text-off", "var(--x-text-off)");
+    ROOT.style.setProperty("--c-line",     "var(--x-line)");
+  }
 
   // Change image
   IMAGES.forEach(i => i.classList.remove("viewing"));

_layouts/gallery.html
@@ -57,6 +57,9 @@ layout: default
         {% if settings.flat or embed %}
           {% assign class = class | append: " flat" %}
         {% endif %}
+        {% if settings.text %}
+          {% assign class = class | append: " " | append: settings.text %}
+        {% endif %}
         {% assign class = class | strip %}
 
         <li class="{{ class }}">

commit 02122ec32e1586f271d81d6c9ad0a788a408c9b5

Fix bug in text colors (closes #6)
11e5060 introduced a bug where requesting a text color different from
the active color scheme would cause text to become illegible in portrait
mode. This change fixes by improving the semantics of the color requests
and only implementing them in landscape.

assets/css/default.css
@@ -17,34 +17,31 @@
   --d-shadow:   rgba(0, 0, 0, 1);
   --d-line:     rgba(255, 255, 255, .2);
 
-  /* defaults for swap */
-  --x-text:     var(--l-text);
-  --x-text-off: var(--l-text-off);
-  --x-line:     var(--l-line);
-
-  /* concrete values */
-  --c-bg:       var(--l-bg);
-  --c-bg-view:  var(--l-bg-view);
-  --c-text:     var(--x-text);
-  --c-text-off: var(--x-text-off);
-  --c-line:     var(--x-line);
-  --c-shadow:   var(--l-shadow);
-
   /* typography */
   --f-size:     15px;
   --f-serif:    "Times New Roman", Times, serif;
   --f-sans:     Arial, sans-serif;
 }
 
+@media (prefers-color-scheme: light) {
+  :root {
+    --c-bg:       var(--l-bg);
+    --c-bg-view:  var(--l-bg-view);
+    --c-text:     var(--l-text);
+    --c-text-off: var(--l-text-off);
+    --c-line:     var(--l-line);
+    --c-shadow:   var(--l-shadow);
+  }
+}
+
 @media (prefers-color-scheme: dark) {
   :root {
     --c-bg:       var(--d-bg);
     --c-bg-view:  var(--d-bg-view);
+    --c-text:     var(--d-text);
+    --c-text-off: var(--d-text-off);
+    --c-line:     var(--d-line);
     --c-shadow:   var(--d-shadow);
-
-    --x-text:     var(--d-text);
-    --x-text-off: var(--d-text-off);
-    --x-line:     var(--d-line);
   }
 }
 

assets/css/landscape.css
@@ -1,3 +1,30 @@
+/* Variables
+ * (handles swap of text colors when request by gallery) */
+
+@media (prefers-color-scheme: light) {
+  :root.d-colors {
+    --c-bg:       var(--d-bg);
+    --c-bg-view:  var(--d-bg-view);
+    --c-text:     var(--d-text);
+    --c-text-off: var(--d-text-off);
+    --c-line:     var(--d-line);
+    --c-shadow:   var(--d-shadow);
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+  :root.l-colors {
+    --c-bg:       var(--l-bg);
+    --c-bg-view:  var(--l-bg-view);
+    --c-text:     var(--l-text);
+    --c-text-off: var(--l-text-off);
+    --c-line:     var(--l-line);
+    --c-shadow:   var(--l-shadow);
+  }
+}
+
+
+
 /* Page structure */
 
 * {

assets/js/gallery.js
@@ -2,7 +2,7 @@
 const ROOT    = document.documentElement,
       GALLERY = document.querySelector("div#gallery"),
       IMAGES  = document.querySelectorAll("div#gallery li"),
-      NAV     = document.querySelectorAll("nav#details li"),
+      NAV   = document.querySelectorAll("nav#details li"),
       A_PREV  = document.querySelector("nav#page-images li.prev"),
       A_NEXT  = document.querySelector("nav#page-images li.next"),
       WAIT    = document.querySelector("p#waiting");
@@ -46,18 +46,13 @@ function change(index) {
 
   // Change text style
   if (IMAGES[index].classList.contains("dark-text")) {
-    ROOT.style.setProperty("--c-text",     "var(--l-text)");
-    ROOT.style.setProperty("--c-text-off", "var(--l-text-off)");
-    ROOT.style.setProperty("--c-line",     "var(--l-line)");
+    ROOT.classList.add("l-colors");
   } else if (IMAGES[index].classList.contains("light-text") ||
       IMAGES[index].classList.contains("dark-bg")) {
-    ROOT.style.setProperty("--c-text",     "var(--d-text)");
-    ROOT.style.setProperty("--c-text-off", "var(--d-text-off)");
-    ROOT.style.setProperty("--c-line",     "var(--d-line)");
+    ROOT.classList.add("d-colors");
   } else {
-    ROOT.style.setProperty("--c-text",     "var(--x-text)");
-    ROOT.style.setProperty("--c-text-off", "var(--x-text-off)");
-    ROOT.style.setProperty("--c-line",     "var(--x-line)");
+    ROOT.classList.remove("l-colors");
+    ROOT.classList.remove("d-colors");
   }
 
   // Change image